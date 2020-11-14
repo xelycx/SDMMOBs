d9483eecf3d74d43d5b179cba7a86185b19884f7
xelycx <xelycx@gmail.com>
2020-11-15 03:52:00 +0800

    database interaction in bug report entity, add controller and view

--- SDMMobs/entity/class.BugReport.php
+++ SDMMobs/entity/class.BugReport.php
@@ -1,7 +1,11 @@
 <?php
+require_once ("DatabaseConfig.php");
+//or use autoloader? //include "autoload.php"
 
 class BugReport
 {
+    private $db_handle;
+    
 	//Attributes
 	public $id;
 	public $title;
@@ -25,6 +29,9 @@ class BugReport
 		$this->closeDate = $closeDate;
 		$this->severityLevel = $severityLevel;
 		$this->comments = $comments;
+		
+		//database
+		$this->db_handle = new DBController();
 	}
 	
 	//Setters
@@ -77,16 +84,77 @@ class BugReport
 	function DeleteBugReport() {}
 	
 	//other functions
-	function ViewBugReports() {}
+	function ViewBugReports()
+	{
+		$query = "SELECT * FROM BugReport";
+		$result = mysqli_query($this->db_handle, $query);
+
+		return $result;
+	}
 	
-	function SearchForBugByKeyword() {}
+	function SearchForBugByKeyword($keyword)
+	{
+		$query = "SELECT * FROM BugReport WHERE description LIKE '%$keyword%' ";
+		$result = mysqli_query($this->db_handle, $query);
+
+		$bugreport = mysqli_fetch_assoc($result);
+		return $bugreport;
+	}
 	
-	function SearchForBugByTitle() {}
+	function SearchForBugByTitle($title)
+	{
+		$query = "SELECT * FROM BugReport WHERE title LIKE '%$title%' ";
+		$result = mysqli_query($this->db_handle, $query);
+
+		$bugreport = mysqli_fetch_assoc($result);
+		return $bugreport;
+	}
 	
-	function SearchForBugByAssignee() {}
+	function SearchForBugByAssignee($assignee)
+	{
+		$query = "SELECT * FROM BugReport WHERE assignee LIKE '%$assignee%' ";
+		$result = mysqli_query($this->db_handle, $query);
+
+		$bugreport = mysqli_fetch_assoc($result);
+		return $bugreport;
+	}
 	
-	function AddComment() {}
-	
+	function AddComment($comment)
+	{	
+		//maybe check in controller??
+		// Check input errors before inserting in database
+		if(empty($comment_err))
+		{
+			// Prepare an insert statement
+			$sql = "INSERT INTO comments (comment, user_id, dateCreated, lastEdited) VALUES (?, ?, NOW(), NOW())";
+			if($stmt = $mysqli->prepare($sql)){
+				// Bind variables to the prepared statement as parameters
+				$stmt->bind_param("si", $comment, $user_id);
+				
+				// Set parameters
+				$param_name = $name;
+				$param_address = $address;
+				$param_salary = $salary;
+				
+				// Attempt to execute the prepared statement
+				if($stmt->execute()){
+					// Comment added successfully. //Redirect to landing page
+					//header("location: index.php");
+					exit();
+				} else{
+					echo "Something went wrong. Please try again later.";
+				}
+			}
+			 
+			// Close statement
+			$stmt->close();
+			
+			/*
+			// Close connection
+			$this->db_handle->close();
+			*/
+		}
+	}
 }
 
 ?>
