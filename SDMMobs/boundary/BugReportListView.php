e5772bbd7d857bca45f3faa9d18a45aa538ba5d7
xelycx <xelycx@gmail.com>
2020-11-16 18:19:17 +0800

    Update BugReportListView.php

--- SDMMobs/boundary/BugReportListView.php
+++ SDMMobs/boundary/BugReportListView.php
@@ -1,6 +1,3 @@
-<?php 
-	include_once("controller/BugReportController.php");
-?>
 <html>
 <head>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
@@ -8,7 +5,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
     <style type="text/css">
         .wrapper{
-            width: 650px;
+            width: 1300px;
             margin: 0 auto;
         }
         .page-header h2{
@@ -27,79 +24,88 @@
 </head>
 
 <body>
-
-<table>
-	<?php
-		if(mysqli_num_rows($bugs)>0)
-		{
-			echo "<table class='table table-bordered table-striped'>";
-				echo "<thead>";
-					echo "<tr>";
-						echo "<th>id</th>";
-						echo "<th>Title</th>";
-						echo "<th>Description</th>";
-						echo "<th>Status</th>";
-						echo "<th>Assignee</th>";
-						echo "<th>Create date</th>";
-						echo "<th>Close date</th>";
-						echo "<th>Severity level</th>";
-						echo "<th>Action</th>";
-					echo "</tr>";
-				echo "</thead>";
-				
-				echo "<tbody>";
-				while($row = mysqli_fetch_assoc($bugs))
-				{
-					echo "<tr>";
-					echo "<td>" . $row['id'] . "</td>";
-					echo "<td>" . $row['title'] . "</td>";
-					echo "<td>" . $row['description'] . "</td>";
-					echo "<td>" . $row['status'] . "</td>";
-					echo "<td>" . $row['assignee'] . "</td>";
-					echo "<td>" . $row['createDate'] . "</td>";
-					echo "<td>" . $row['closeDate'] . "</td>";
-					echo "<td>" . $row['severityLevel'] . "</td>";
-					echo "<td>";
-						echo "<a href='read.php?id=". $row['id'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
-						echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
-						echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
-					echo "</td>";
+<div class="wrapper">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<table>
+					<?php
+						include_once("controller/BugReportController.php");
 					
-					echo "</tr>";
-				}
-				echo "</tbody>";
-			echo "</table>";
-			
-			echo "Number of bug reports: " . mysqli_num_rows($bugs);
-			
-			// foreach ($bugs as $title => $bug)
-			// {
-				// echo '<tr>
-				// <td>
-				// <a href="indexbugreportlist.php?bug='.$bug->title.'">'.$bug->stripslashes(title).'
-				// </a>
-				// </td>
-				// <td>'.$bug->description.'
-				// </td>
-				// <td>'.$bug->status.'
-				// </td>
-				// <td>'.$bug->assignee.'
-				// </td>
-				// <td>'.$bug->Status.'
-				// </td>
-				// <td>'.$bug->createDate.'
-				// </td>
-				// <td>'.$bug->closeDate.'
-				// </td>
-				// <td>'.$bug->severityLevel.'
-				// </td>
-				// </tr>';
-			// }
-			
-		}
+						if(mysqli_num_rows($bugs)>0)
+						{
+							echo "<table class='table table-bordered table-striped'>";
+								echo "<thead>";
+									echo "<tr>";
+										echo "<th>id</th>";
+										echo "<th>Title</th>";
+										echo "<th>Description</th>";
+										echo "<th>Status</th>";
+										echo "<th>Assignee</th>";
+										echo "<th>Create date</th>";
+										echo "<th>Close date</th>";
+										echo "<th>Severity level</th>";
+										echo "<th>Action</th>";
+									echo "</tr>";
+								echo "</thead>";
+								
+								echo "<tbody>";
+								while($row = mysqli_fetch_assoc($bugs))
+								{
+									echo "<tr>";
+									echo "<td>" . $row['id'] . "</td>";
+									echo "<td>" . $row['title'] . "</td>";
+									echo "<td>" . $row['description'] . "</td>";
+									echo "<td>" . $row['status'] . "</td>";
+									echo "<td>" . $row['assignee'] . "</td>";
+									echo "<td>" . $row['createDate'] . "</td>";
+									echo "<td>" . $row['closeDate'] . "</td>";
+									echo "<td>" . $row['severityLevel'] . "</td>";
+									echo "<td>";
+										echo "<a href='read.php?id=". $row['id'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
+										echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+										echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+									echo "</td>";
+									
+									echo "</tr>";
+								}
+								echo "</tbody>";
+							echo "</table>";
+							
+							echo "Number of bug reports: " . mysqli_num_rows($bugs);
+							
+							// foreach ($bugs as $title => $bug)
+							// {
+								// echo '<tr>
+								// <td>
+								// <a href="indexbugreportlist.php?bug='.$bug->title.'">'.$bug->stripslashes(title).'
+								// </a>
+								// </td>
+								// <td>'.$bug->description.'
+								// </td>
+								// <td>'.$bug->status.'
+								// </td>
+								// <td>'.$bug->assignee.'
+								// </td>
+								// <td>'.$bug->Status.'
+								// </td>
+								// <td>'.$bug->createDate.'
+								// </td>
+								// <td>'.$bug->closeDate.'
+								// </td>
+								// <td>'.$bug->severityLevel.'
+								// </td>
+								// </tr>';
+							// }
+							
+						}
 
-	?>
-</table>
+					?>
+				</table>
+			</div>
+		</div>        
+	</div>
+</div>
 
 </body>
 </html>
