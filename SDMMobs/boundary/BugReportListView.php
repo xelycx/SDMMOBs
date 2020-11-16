71c412433ea285b0d4176d167c29912a75a48c54
xelycx <xelycx@gmail.com>
2020-11-16 18:02:09 +0800

    Update BugReportListView.php

--- SDMMobs/boundary/BugReportListView.php
+++ SDMMobs/boundary/BugReportListView.php
@@ -30,69 +30,73 @@
 
 <table>
 	<?php
-		echo "<table class='table table-bordered table-striped'>";
-			echo "<thead>";
-				echo "<tr>";
-					echo "<th>id</th>";
-					echo "<th>Title</th>";
-					echo "<th>Description</th>";
-					echo "<th>Status</th>";
-					echo "<th>Assignee</th>";
-					echo "<th>Create date</th>";
-					echo "<th>Close date</th>";
-					echo "<th>Severity level</th>";
-					echo "<th>Action</th>";
-				echo "</tr>";
-			echo "</thead>";
-			
-			echo "<tbody>";
-			while($row = mysqli_fetch_assoc($bugs))
-			{
-				echo "<tr>";
-				echo "<td>" . $row['id'] . "</td>";
-				echo "<td>" . $row['title'] . "</td>";
-				echo "<td>" . $row['description'] . "</td>";
-				echo "<td>" . $row['status'] . "</td>";
-				echo "<td>" . $row['assignee'] . "</td>";
-				echo "<td>" . $row['createDate'] . "</td>";
-				echo "<td>" . $row['closeDate'] . "</td>";
-				echo "<td>" . $row['severityLevel'] . "</td>";
-				echo "<td>";
-					echo "<a href='read.php?id=". $row['id'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
-					echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
-					echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
-				echo "</td>";
+		if(mysqli_num_rows($bugs)>0)
+		{
+			echo "<table class='table table-bordered table-striped'>";
+				echo "<thead>";
+					echo "<tr>";
+						echo "<th>id</th>";
+						echo "<th>Title</th>";
+						echo "<th>Description</th>";
+						echo "<th>Status</th>";
+						echo "<th>Assignee</th>";
+						echo "<th>Create date</th>";
+						echo "<th>Close date</th>";
+						echo "<th>Severity level</th>";
+						echo "<th>Action</th>";
+					echo "</tr>";
+				echo "</thead>";
 				
-				echo "</tr>";
-			}
-			echo "</tbody>";
-		echo "</table>";
-		
-		echo "Number of bug reports: " . mysqli_num_rows($bugs);
-		
-		// foreach ($bugs as $title => $bug)
-		// {
-			// echo '<tr>
-			// <td>
-			// <a href="indexbugreportlist.php?bug='.$bug->title.'">'.$bug->stripslashes(title).'
-			// </a>
-			// </td>
-			// <td>'.$bug->description.'
-			// </td>
-			// <td>'.$bug->status.'
-			// </td>
-			// <td>'.$bug->assignee.'
-			// </td>
-			// <td>'.$bug->Status.'
-			// </td>
-			// <td>'.$bug->createDate.'
-			// </td>
-			// <td>'.$bug->closeDate.'
-			// </td>
-			// <td>'.$bug->severityLevel.'
-			// </td>
-			// </tr>';
-		// }
+				echo "<tbody>";
+				while($row = mysqli_fetch_assoc($bugs))
+				{
+					echo "<tr>";
+					echo "<td>" . $row['id'] . "</td>";
+					echo "<td>" . $row['title'] . "</td>";
+					echo "<td>" . $row['description'] . "</td>";
+					echo "<td>" . $row['status'] . "</td>";
+					echo "<td>" . $row['assignee'] . "</td>";
+					echo "<td>" . $row['createDate'] . "</td>";
+					echo "<td>" . $row['closeDate'] . "</td>";
+					echo "<td>" . $row['severityLevel'] . "</td>";
+					echo "<td>";
+						echo "<a href='read.php?id=". $row['id'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
+						echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+						echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+					echo "</td>";
+					
+					echo "</tr>";
+				}
+				echo "</tbody>";
+			echo "</table>";
+			
+			echo "Number of bug reports: " . mysqli_num_rows($bugs);
+			
+			// foreach ($bugs as $title => $bug)
+			// {
+				// echo '<tr>
+				// <td>
+				// <a href="indexbugreportlist.php?bug='.$bug->title.'">'.$bug->stripslashes(title).'
+				// </a>
+				// </td>
+				// <td>'.$bug->description.'
+				// </td>
+				// <td>'.$bug->status.'
+				// </td>
+				// <td>'.$bug->assignee.'
+				// </td>
+				// <td>'.$bug->Status.'
+				// </td>
+				// <td>'.$bug->createDate.'
+				// </td>
+				// <td>'.$bug->closeDate.'
+				// </td>
+				// <td>'.$bug->severityLevel.'
+				// </td>
+				// </tr>';
+			// }
+			
+		}
 
 	?>
 </table>
