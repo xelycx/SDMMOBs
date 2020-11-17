4a3d75338b98c8000d4511c89735b71698a1b97b
xelycx <xelycx@gmail.com>
2020-11-17 23:01:00 +0800

    report generator export csv
    not done/tested

---
+++ SDMMobs/export.php
@@ -0,0 +1,81 @@
+<?php
+
+function array2csv(array &$array)
+{
+   if (count($array) == 0) {
+     return null;
+   }
+   ob_start();
+   $df = fopen("php://output", 'w');
+   fputcsv($df, array_keys(reset($array)));
+   foreach ($array as $row) {
+      fputcsv($df, $row);
+   }
+   fclose($df);
+   return ob_get_clean();
+}
+
+function download_send_headers($filename) {
+    // disable caching
+    $now = gmdate("D, d M Y H:i:s");
+    header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+    header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
+    header("Last-Modified: {$now} GMT");
+
+    // force download  
+    header("Content-Type: application/force-download");
+    header("Content-Type: application/octet-stream");
+    header("Content-Type: application/download");
+
+    // disposition / encoding on response body
+    header("Content-Disposition: attachment;filename={$filename}");
+    header("Content-Transfer-Encoding: binary");
+}
+
+download_send_headers("data_export_" . date("Y-m-d") . ".csv");
+echo array2csv($array);
+die();
+
+/*
+include 'DatabaseConfig.php';
+$date=date("Y-m-d");
+
+   // echo intval(date("W", $date)) - intval(date("W", $firstOfMonth)) + 1;
+
+$month= date("F", strtotime($date)); //May
+$week=date("W", strtotime($date)); //May
+
+//get records from database
+$query = $db->query("SELECT * FROM bugreport where month = '$month'");
+
+
+    $delimiter = ",";
+    $filename = "monthlyReport_" . date('Y-m-d') . ".csv";
+    
+    //create a file pointer
+    $f = fopen('php://memory', 'w');
+    
+    //set column headers
+    $fields = array('title', 'description', 'status', 'assignee');
+    fputcsv($f, $fields, $delimiter);
+    
+    //output each row of the data, format line as csv and write to file pointer
+    while($row = $query->fetch_assoc()){
+             $lineData = array($row['title'],$row['description'],$row['status'],$row['assignee']);
+        fputcsv($f, $lineData, $delimiter);
+    }
+    
+    //move back to beginning of file
+    fseek($f, 0);
+    
+    //set headers to download file rather than displayed
+    header('Content-Type: text/csv');
+    header('Content-Disposition: attachment; filename="' . $filename . '";');
+    
+    //output all remaining data on a file pointer
+    fpassthru($f);
+
+exit;
+*/
+
+?>
